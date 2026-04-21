073e171a81799361e6584d52631546fabd5c30d9
sfr2645 <sfr2645@email.vccs.edu>
2026-04-21 18:19:13 -0400

    Add project files.

---
+++ M5_Trees_and_Priority.py
@@ -0,0 +1,30 @@
+from binary_expression_tree import BinaryExpressionTree
+
+def main():
+    postfix_list = [
+        "5 3 +",
+        "8 2 - 3 +",
+        "5 3 8 * +",
+        "6 2 / 3 +",
+        "5 8 + 3 -",
+        "5 3 + 8 *",
+        "8 2 3 * + 6 -",
+        "5 3 8 * + 2 /",
+        "8 2 + 3 6 * -",
+        "5 3 + 8 2 / -"
+    ]
+
+    print("----- Binary Expression Tree -----")
+
+    for expr in postfix_list:
+        tree = BinaryExpressionTree()
+        tree.build_tree(expr)
+
+        print("Infix Expression:", tree.inorder())
+        print("Postfix Expression:", tree.postorder())
+        print("Evaluated Result:", tree.evaluate())
+        print()
+
+
+if __name__ == "__main__":
+    main()
