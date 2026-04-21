713f8225c238a1569d5be60a9eecbc8611ab9d0b
sfr2645 <sfr2645@email.vccs.edu>
2026-04-21 18:20:55 -0400

    create stack class

---
+++ stack.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        raise IndexError("Pop from empty stack")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        raise IndexError("Peek from empty stack")
+
+    def is_empty(self):
+        return len(self.items) == 0
